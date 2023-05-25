616134c2fd29a8578f7cce3f3064bd4b4ea7b9db
João Victor Rocha Santos <rocha.joao.victor.50@gmail.com>
2023-05-25 09:20:25 -0300

    Teste de api para funcionamento dentro da página

--- 3_projeto/components/Card/index.jsx
+++ 3_projeto/components/Card/index.jsx
@@ -7,9 +7,8 @@ export default function Card({ pokemon }) {
     return (
         <div className={styles.card}>
             <Image 
-                // src={`https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png`}
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png
-                `}
+                src={`https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png`}
+                // src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
                 width={120}
                 height={120}
                 alt={pokemon.name}
